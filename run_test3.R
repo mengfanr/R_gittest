53c5b8727d904d2975574d5fb7e9b92f1c1c3757
mengfanr <mengfanr0306@gmail.com>
2023-12-15 00:23:24 +0800

    data cleaning test

--- run_test3.R
+++ run_test3.R
@@ -1,8 +1,13 @@
-day_number = 2
-day_number_base = as.character(day_number)
-day_base = "DAY"
-day_dir = paste(day_base,day_number_base,sep='')
-if (!dir.exists(day_dir))
-  dir.create(day_dir)
-setwd(day_dir)
+# day_number = 2
+# day_number_base = as.character(day_number)
+# day_base = "DAY"
+# day_dir = paste(day_base,day_number_base,sep='')
+# if (!dir.exists(day_dir))
+#   dir.create(day_dir)
+# install.packages(tidyverse)
+library(tidyverse)
+counts = read.table(file='TCGA-LIHC.htseq_counts.tsv', sep='\t', header=TRUE)
+rownames(counts)  = counts[,1] 
+counts = counts[,  -1]
+
 
